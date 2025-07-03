a7514081ee3bb97120ed0ce341bcbc1054904e41
enosiam <emmanueleffiong149@gmail.com>
2025-07-03 19:09:47 +0100

    commit

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,8 +1,14 @@
 using System;
 
-Console.Write("What is your name? ");
-string first = Console.ReadLine();
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("What is your name? ");
+        string first = Console.ReadLine();
 
-Console.Write("What is your last name? ");
-string last = Console.ReadLine();
-Console.WriteLine($"Your name is, {first} {last}");
+        Console.Write("What is your last name? ");
+        string last = Console.ReadLine();
+        Console.WriteLine($"Your name is, {first} {last}");
+    }
+}
