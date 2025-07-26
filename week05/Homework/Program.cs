582f65d2e0ec8455bfd6698aed523970f629ce67
enosiam <emmanueleffiong149@gmail.com>
2025-07-26 17:07:48 +0100

    Homework

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        // Base assignment test
+        Assignment basic = new Assignment("Samuel Esik", "Multiplication");
+        Console.WriteLine(basic.GetSummary());
+        Console.WriteLine();
+
+        // Math assignment test
+        MathAssignment math = new MathAssignment("Emmanuel Effiong", "Fractions", "7.3", "8-19");
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
+        Console.WriteLine();
+
+        // Writing assignment test
+        WritingAssignment writing = new WritingAssignment("Jesuina Emmanuel", "Latter-Day Saint History", "Joseph Smith's First Vision");
+        Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(writing.GetWritingInformation());
     }
 }
